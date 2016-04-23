9b2c7e69f5661d64f7367e779da712a7f1931c71
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-04-23 17:49:22 +0200

    use CONMGR_GROUP environment variable

--- server/conmgr-server/docker_ops.py
+++ server/conmgr-server/docker_ops.py
@@ -1,12 +1,16 @@
+import os
 import docker
 
 cli = docker.Client(base_url='unix://var/run/docker.sock')
 
-filters = {'label':['com.conmgr.name', 'com.conmgr.group']}
+filters = {'label':['com.conmgr.name']}
 
 def getContainers():
     containers = cli.containers(all=True, filters=filters)
-    containers_filtered = [x for x in containers if x['Labels']['com.conmgr.group'] == 'smalle-voet']
+    containers_filtered = [x for x in containers if
+        (not 'CONMGR_GROUP' in os.environ) or
+        ('com.conmgr.group' in x['Labels'] and
+         x['Labels']['com.conmgr.group'] == os.environ['CONMGR_GROUP'])]
     containers_transformed = [
         {
             'id': x['Id'],
