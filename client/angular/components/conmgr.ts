59362b1197c2995a09c800f11a8951a3f6805227
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-04-23 17:48:34 +0200

    sort by name

--- client/angular/components/conmgr.ts
+++ client/angular/components/conmgr.ts
@@ -43,7 +43,11 @@ export class ConmgrComponent implements OnInit {
   refresh() {
     this.pending = true;
     this._containerService.getContainers().subscribe(
-      containers => this.containers = containers,
+      containers => this.containers = containers.sort((a, b) => {
+        if(a.name < b.name) return -1;
+        if(a.name > b.name) return 1;
+        return 0;
+      }),
       err => this.addAlert('Refresh failed!'),
       () => this.pending = false
     );
