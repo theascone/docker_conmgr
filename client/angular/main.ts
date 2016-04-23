5eb076efd94e9571a23e4c3030004be386d3c4a1
Addis Dittebrandt <addis.dittebrandt@gmail.com>
2016-04-23 17:48:14 +0200

    enableProdMode

--- client/angular/main.ts
+++ client/angular/main.ts
@@ -2,7 +2,9 @@
 
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
+import {enableProdMode} from "angular2/core";
 
 import {ConmgrComponent} from './components/conmgr';
 
+enableProdMode();
 bootstrap(ConmgrComponent, [ HTTP_PROVIDERS ]);
